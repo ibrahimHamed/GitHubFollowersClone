c4728233907745604ca238b5c679e084b1e93d29
Ibrahim Hamed <ibrahim@Ibrahims-MacBook-Pro.local>
2022-01-18 05:36:48 +0200

    Design search screen

--- GitHubFollowersClone/SearchVC.swift
+++ GitHubFollowersClone/SearchVC.swift
@@ -8,22 +8,57 @@
 import UIKit
 
 class SearchVC: UIViewController {
+    
+    let logoImageView = UIImageView()
+    let usernameTF = GFTextField()
+    let actionButton = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        configureLogo()
+        configureTF()
+        configureActionButton()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+//        navigationController?.isNavigationBarHidden = true
     }
-    */
+    
+    private func configureLogo(){
+        view.addSubview(logoImageView)
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.image = UIImage(named: "gh-logo")!
+        
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.heightAnchor.constraint(equalToConstant: 200),
+            logoImageView.widthAnchor.constraint(equalToConstant: 200)
+        ])
+    }
+    
+    private func configureTF(){
+        view.addSubview(usernameTF)
+        
+        NSLayoutConstraint.activate([
+            usernameTF.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 48),
+            usernameTF.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            usernameTF.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            usernameTF.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    private func configureActionButton(){
+        view.addSubview(actionButton)
+        
+        NSLayoutConstraint.activate([
+            actionButton.heightAnchor.constraint(equalToConstant: 50),
+            actionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            actionButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            actionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50)
+        ])
+    }
 
 }
